d96c60402b2af2bf079ff7334781fcd02a8f28a3
imalok333 <alokssystem333@gmail.com>
2024-11-15 19:17:33 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import LoginForm from './LoginForm';
+import Register from './Register';
+
+function App() {
+  return (
+    <div>
+      <Register />
+      
+    </div>
+  );
+}
+
+export default App;
